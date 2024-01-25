7ac0a9212f6ccb25142441317aa78997c5e39f54
Maxim <odi.max2002@gmail.com>
2024-01-25 13:08:42 +0300

    fix tree view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./templates/*.html",
-    "./static/*.css",
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/tw-elements/dist/js/**/*.js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin'),
-    require("tw-elements/dist/plugin.cjs")
-  ],
-}
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./templates/*.html",
+//     "./static/*.css",
+//     "./node_modules/flowbite/**/*.js",
+//     "./node_modules/tw-elements/dist/js/**/*.js"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require("tw-elements/dist/plugin"),
+//     require('flowbite/plugin'),
+//   ],
+// }
 
